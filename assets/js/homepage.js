c7e5bf121a0e5a83950232b40f6564e1cd17ddfc
polizoto <polizoto@hotmail.com>
2021-12-22 18:40:35 -0500

    Added API calls to Open Library

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,82 @@
 var authorFormEl = document.querySelector("#author-form");
 var authorInputEl = document.querySelector("#author");
 var bookContainerEl = document.querySelector("#books-container");
+var bookInfoEl = ""
+var bookPublisher = ""
+var bookDate = ""
+
+var getBookInfo = function(bookID) {
+
+  // var apiUrl = "https://openlibrary.org/api/books?bibkeys=OLID:" + bookID + "&format=json";
+  var apiUrl = "https://openlibrary.org/api/volumes/brief/olid/" + bookID + ".json";
+
+  fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data);
+        // bookPublisher = data.records./books/bookID.data.publishers.name
+        // bookDate = data.records./books/bookID.publishDates
+        // console.log(bookPublisher);
+        // console.log(publishDates);
+      });
+    } else {
+      alert("Error: " + bookID + " not found at Open Library");
+    }
+  })
+    .catch(function(error) {
+      console.log('error ', error)
+     // alert("Unable to connect to Open Library API");
+    });
+
+}
+
+var getBookAvailability = function(bookAuthor, bookString, bookSubject, bookInfoEl) {
+
+var apiUrl = "https://openlibrary.org/search.json?author=" + bookAuthor + "&title=" + bookString + "&subject=" + bookSubject;
+
+fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log('DATA ', data)
+
+        var bookAvailabilityEl = document.createElement("p");
+        var bookListEl = document.createElement("ol");
+        for (let i = 0; i < data.docs.length; i++) {
+          var bookID = data.docs[i].seed[0].replace("/books/", "")
+          
+          var bookInstanceEl = document.createElement("li");
+          // if (data.docs[i].isbn) {
+          //   var bookISBN = data.docs[i].isbn[0]
+          //   bookInstanceEl.setAttribute("data-ISBN", bookISBN) 
+          // }
+          // var bookPublisher = data.docs[i].publisher[0]
+          // if (bookPublisher.indexOf(',') > -1) 
+          // {bookPublisher = bookPublisher.substring(1, bookPublisher.indexOf(',')) }
+          // if (data.docs[i].id_goodreads) {
+          //   bookInstanceEl.setAttribute("data-goodreads", data.docs[i].id_goodreads)
+          // }
+
+          getBookInfo(bookID)
+
+          bookInstanceEl.setAttribute("data-OLID", bookID)
+          // bookInstanceEl.textContent = bookPublisher + " (" + publishDates + ")"
+          bookListEl.appendChild(bookInstanceEl)
+          bookAvailabilityEl.appendChild(bookListEl)
+        }
+        bookInfoEl.appendChild(bookAvailabilityEl);
+      });
+    } else {
+      alert("Error: " + bookID + " not found at Open Library");
+    }
+  })
+    .catch(function(error) {
+      alert("Unable to connect to Open Library API");
+    });
+
+}
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -15,9 +91,9 @@ var formSubmitHandler = function(event) {
     }
   };
 
-  var displayBooks = function(author) {
+  var displayBooks = function(author, authorKey) {
 
-    var apiUrl = "https://openlibrary.org/authors/" + author + "/works.json";
+  var apiUrl = "https://openlibrary.org/authors/" + authorKey + "/works.json?limit=200";
 
 fetch(apiUrl)
   .then(function(response) {
@@ -28,9 +104,21 @@ fetch(apiUrl)
         for (let i = 0; i < data.entries.length; i++) {
             var bookEl = document.createElement("p");
             var bookName = data.entries[i].title;
+            var bookString = encodeURIComponent(data.entries[i].title.replace(/-/g,'').trim())
+            if (data.entries[i].subjects) {
+            var bookSubject = encodeURIComponent(data.entries[i].subjects[0].replace(/-/g,'').trim());
+            } else {
+              var bookSubject = "\"\""
+            }
+            var bookAuthor = author.toLowerCase().replaceAll(" ", "+")
+            bookEl.setAttribute("id", "work-" + i)
             bookEl.textContent = bookName;
             bookContainerEl.appendChild(bookEl);
+            bookInfoEl = document.querySelector("#work-" + i);
+            getBookAvailability(bookAuthor, bookString, bookSubject, bookInfoEl)
         }
+        // Sort results alphabetically or by date published
+
       });
     } else {
       alert("Error: " + author + "'s books not found at Open Library");
@@ -53,7 +141,7 @@ fetch(apiUrl)
         // console.log(data);
         var authorKey = data.docs[0].key;
         // do something here
-        displayBooks(authorKey);
+        displayBooks(author, authorKey);
       });
     } else {
       alert("Error: Author Not Found");
